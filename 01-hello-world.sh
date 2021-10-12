81d5a4a9b4b8faeb6a3e000885a5cf92f4202d7f
Marcelo Freire <marcelo.freire.silva@gmail.com>
2021-10-12 01:33:11 -0300

    Add the exercise as a comment on 01-hello-world.sh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# =====================================================
+# Exercise
+#
+# Use the "echo" command to print the line "Hello, World!".
+# =====================================================
+
 # Text to the right of a '#' is treated as a comment - below is the shell command
 
-echo 'Hello, World!'
+echo "Hello, World!"
